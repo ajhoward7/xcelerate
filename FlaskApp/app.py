2b29f1dc89da9ebb324f9a73410aff713d68cda0
danaiavg <dsavgerinou@dons.usfca.edu>
2018-04-05 09:30:05 -0700

    Flask app sign up configurations

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, json
 import csv
 
-file_path = '/Users/danaiavg/Desktop/App_Developemnt/xcelerate/users/master_users.csv'
+file_path = '../users/master_users.csv'
 
 app = Flask(__name__)
 
@@ -27,18 +27,32 @@ def signUp():
 
     # validate the received values
     if _name and _email and _password:
+        print(_name)
         # write in the users file
         myData = []
         myData.append([_name, _email, _password])
 
-        myFile = open(file_path, 'a')
-        writer = csv.writer(myFile)
-        writer.writerows(myData)
+        myFile = open(file_path, 'r+')
+
+        reader = csv.reader(myFile, delimiter=',')
+        flag = 'Not in file'
+        # check database whether the user's name or email already exists
+        for row in reader:
+            if _name == row[0] or _email == row[1]:
+                flag = 'In file'
+                break
+
+        # write only if the user's email or name does not exist
+        if flag == 'Not in file':
+            writer = csv.writer(myFile)
+            writer.writerows(myData)
+        else:
+            return json.dumps({'html': '<span>Enter the required fields</span>'})
+
         myFile.close()
 
-        #### We need to check is the username and the email already exist
-
-        return render_template('home.html', username=_name)
+        # return render_template('home.html', username=_name)
+        return json.dumps({'html': '<span>Good job</span>'})
     else:
         return json.dumps({'html': '<span>Enter the required fields</span>'})
 
@@ -46,8 +60,6 @@ def signUp():
 def home(username):
     return render_template('home.html', username=username)
 
-# @app.route('/logIn')
-# def logIn():
 
 if __name__ == "__main__":
     app.run()
