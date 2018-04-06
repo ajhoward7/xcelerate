75af0bd2869e9000089933164804e66316a6483d
danaiavg <dsavgerinou@dons.usfca.edu>
2018-04-05 18:42:07 -0700

    updated login

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, json, Response, redirect, url_for, request, session, abort
-from flask_login import LoginManager, UserMixin,login_required, login_user, logout_user
+from flask import Flask, render_template, request
 import csv
 
 file_path = '../users/master_users.csv'
@@ -10,48 +9,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/showlogin")
-def showlogin():
+@app.route("/login")
+def login():
     return render_template('login.html')
 
-####################
-@app.route("/login", methods=["POST"])
-def login():
-    print(1)
+@app.route("/home2",methods=["POST"])
+def home2():
     username = request.form['inputName']
     password = request.form['inputPassword']
-
     myFile = open(file_path, 'r+')
-
     reader = csv.reader(myFile, delimiter=',')
-    flag = 'Not in file'
+
     # check database whether the user's name or email already exists
     for row in reader:
         if (username == row[0] or username == row[1]) and password == row[2]:
-            flag = 'In file'
+            return render_template('home.html', name=row[0])
 
-            break
-
-    print(flag)
     myFile.close()
-
-    if flag == 'In file':
-        # return render_template('home.html', username=_name)
-        return json.dumps({'html': '<span>Good job</span>'})
-    else:
-        return json.dumps({'html': '<span>Enter the required fields</span>'})
-
-
-
-
-        # if password == username + "_secret":
-        #     id = username.split('user')[1]
-        #     user = User(id)
-        #     login_user(user)
-        #     return redirect(request.args.get("next"))
-        # else:
-        #     return abort(401)
-####################
+    return render_template('login.html')
 
 @app.route("/showSignup")
 def showSignup():
@@ -66,7 +41,6 @@ def home():
 
     # validate the received values
     if _name and _email and _password:
-        # print(_name)
         # write in the users file
         myData = []
         myData.append([_name, _email, _password])
@@ -74,19 +48,15 @@ def home():
         myFile = open(file_path, 'r+')
 
         reader = csv.reader(myFile, delimiter=',')
-        flag = 'Not in file'
         # check database whether the user's name or email already exists
         for row in reader:
             if _name == row[0] or _email == row[1]:
-                flag = 'In file'
+                # flag = 'In file'
                 return render_template('signup.html')
 
         # write only if the user's email or name does not exist
-        if flag == 'Not in file':
-            writer = csv.writer(myFile)
-            writer.writerows(myData)
-        # else:
-        #     return json.dumps({'html': '<span>Enter the required fields</span>'})
+        writer = csv.writer(myFile)
+        writer.writerows(myData)
 
         myFile.close()
 
