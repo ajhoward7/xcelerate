3c7ca1e60c38b7293e637f284a779a24a92c7ced
danaiavg <dsavgerinou@dons.usfca.edu>
2018-04-10 19:34:22 -0700

    creating the folder and the json for the user

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 import csv
+import json
 
-mater_file_path = '../users/master_users.csv'
+master_file_path = '../users/master_users.csv'
+users_folder_file_path = '../users/'
 
 app = Flask(__name__)
 
@@ -13,7 +16,7 @@ def login():
 def authenticate():
     username = request.form['inputName']
     password = request.form['inputPassword']
-    myFile = open(mater_file_path, 'r+')
+    myFile = open(master_file_path, 'r+')
     reader = csv.reader(myFile, delimiter=',')
 
     # check database whether the user's name or email already exists
@@ -32,9 +35,8 @@ def home(username):
 def showSignup():
     return render_template('signup.html')
 
-@app.route('/racetype', methods=['POST'])
-def racetype():
-    # read the posted values from the UI
+@app.route("/createUsers", methods=["POST"])
+def createUsers():
     username = request.form['inputName']
     email = request.form['inputEmail']
     password = request.form['inputPassword']
@@ -45,14 +47,13 @@ def racetype():
         myData = []
         myData.append([username, email, password])
 
-        myFile = open(mater_file_path, 'r+')
+        myFile = open(master_file_path, 'r+')
 
         reader = csv.reader(myFile, delimiter=',')
         # check database whether the user's name or email already exists
         for row in reader:
             if username == row[0] or email == row[1]:
-
-                return render_template('signup.html')
+                return redirect(url_for('showSignup'))
 
         # write only if the user's email or name does not exist
         writer = csv.writer(myFile)
@@ -60,39 +61,78 @@ def racetype():
 
         myFile.close()
 
-        # return render_template('home.html', username=_name)
-        return render_template('racetype.html')
+        new_path = users_folder_file_path + username
+        os.makedirs(new_path)
+
+        return redirect(url_for('.racetype', username=username))
     else:
-        return render_template('signup.html')
+        return redirect(url_for('showSignup'))
 
-@app.route("/daysperweek", methods=['POST'])
-def daysperweek():
-    ###
-    print(request.form['typerace'])
-    ###
-    return render_template('daysperweek.html')
 
-@app.route("/runlevel", methods=['POST'])
-def runlevel():
-    ###
-    print(request.form.getlist('dayofweek'))
-    ###
-    return render_template('runerlevel.html')
+@app.route('/racetype/<username>')
+def racetype(username):
+    return render_template('racetype.html', username=username)
 
-@app.route("/dates", methods=['POST'])
-def dates():
-    ###
-    print(request.form['runlevel'])
-    ###
-    return render_template('dates.html')
+@app.route("/daysperweek/<username>", methods=['POST'])
+def daysperweek(username):
 
-@app.route("/home2",methods=["POST"])
-def backhome():
-    ###
-    print(request.form['startdate'])
-    print(request.form['enddate'])
-    ###
-    return render_template('home.html')
+    new_path = users_folder_file_path + username
+
+    data = {}
+    data['username'] = username
+    data['typerace'] = request.form['typerace']
+    data['dayofweek'] = ''
+    data['runlevel'] = ''
+    data['startdate'] = ''
+    data['enddate'] = ''
+
+    with open(new_path + '/preferences.txt', 'w') as outfile:
+        json.dump(data, outfile)
+
+    return render_template('daysperweek.html', username=username)
+
+@app.route("/runlevel/<username>", methods=['POST'])
+def runlevel(username):
+
+    with open(users_folder_file_path + username +'/preferences.txt', 'r+') as json_file:
+        data = json.load(json_file)
+
+        data["dayofweek"] = request.form.getlist('dayofweek')
+
+        json_file.seek(0)  # rewind
+        json.dump(data, json_file)
+        json_file.truncate()
+
+    return render_template('runerlevel.html', username=username)
+
+@app.route("/dates/<username>", methods=['POST'])
+def dates(username):
+
+    with open(users_folder_file_path + username +'/preferences.txt', 'r+') as json_file:
+        data = json.load(json_file)
+
+        data["runlevel"] = request.form['runlevel']
+
+        json_file.seek(0)  # rewind
+        json.dump(data, json_file)
+        json_file.truncate()
+
+    return render_template('dates.html', username=username)
+
+@app.route("/backhome/<username>",methods=["POST"])
+def backhome(username):
+
+    with open(users_folder_file_path + username +'/preferences.txt', 'r+') as json_file:
+        data = json.load(json_file)
+
+        data["startdate"] = request.form['startdate']
+        data["enddate"] = request.form['enddate']
+
+        json_file.seek(0)  # rewind
+        json.dump(data, json_file)
+        json_file.truncate()
+
+    return redirect(url_for('.home', username=username))
 
 if __name__ == "__main__":
     app.run()
