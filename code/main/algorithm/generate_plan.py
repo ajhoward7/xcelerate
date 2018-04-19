10e48303015392b00339af48ead9994d3f0a0c10
Alex Howard <Alex@Alexs-MacBook-Pro.local>
2018-04-19 13:36:02 -0700

    Code structure change

--- code/main/algorithm/generate_plan.py
+++ code/main/algorithm/generate_plan.py
@@ -1,49 +1,6 @@
-from datetime import * # Used in how_many_weeks_left
-
 import constants
 import lib
-
-
-def generate_training_plan(user):
-    """
-    Steps:
-    1. Read in users preferences
-    2. Choose appropriate run vector
-    3. Generate training plan
-    """
-    preferences = lib.get_preferences(user)  # Jake
-
-    run_vector = lib.get_run_vector(user)  # Jake
-
-    training_plan = build_plan(preferences, run_vector)
-
-    return training_plan
-
-
-def build_plan(preferences, run_vector):
-    """
-    Steps:
-    1. Decide how many days to run on each week up until race day
-    2. Decide how many miles to run on each week
-    3. Select appropriate proportion of run vector
-    4. Return plan, divided by week
-    """
-
-    weeks_of_plan = how_many_weeks_left(preferences['race_date'], preferences['start_date'])  # Jake
-
-    days_per_week = days_per_week(preferences, weeks_of_plan)  # Holly
-
-    mileage_baseline = get_baseline_mileage(preferences)
-    mileage_limit = get_limit_mileage(preferences)
-
-    if mileage_limit <= mileage_baseline + 5:
-        mileage_limit = mileage_baseline + 5
-
-    miles_per_week = generate_mpw(mileage_baseline, mileage_limit, weeks_of_plan)
-
-    training_plan = construct_plan(days_per_week, miles_per_week, run_vector, weeks_of_plan)
-
-    return training_plan
+import utils
 
 
 def generate_mpw(mileage_baseline, mileage_limit, weeks_of_plan):
@@ -68,72 +25,7 @@ def generate_mpw(mileage_baseline, mileage_limit, weeks_of_plan):
     return miles_per_week
 
 
-def get_baseline_mileage(preferences):
-    """
-    1. Novice, no prior training - pre-defined constants
-    2. Novice, prior training - prior training levels
-    3. Intermediate - prior training levels
-    """
-
-    if 'prior_miles_per_week' in preferences.keys():
-        return preferences['prior_miles_per_week']
-    else:
-        if preferences['race_distance'] == 0:
-            return constants.novice_5k_baseline
-        if preferences['race_distance'] == 1:
-            return constants.novice_10k_baseline
-        if preferences['race_distance'] == 2:
-            return constants.novice_half_baseline
-        if preferences['race_distance'] == 3:
-            return constants.novice_full_baseline
-
-
-def get_limit_mileage(preferences):
-    """
-    1. Novice - pre-defined constants
-    2. Intermediate, keep same levels - set to previous level
-    3. Intermediate, increase level - pre-defined constants
-    """
-
-    if preferences['runner_type'] == 0:
-        if preferences['race_distance'] == 0:
-            return constants.novice_5k_limit
-        if preferences['race_distance'] == 1:
-            return constants.novice_10k_limit
-        if preferences['race_distance'] == 2:
-            return constants.novice_half_limit
-        if preferences['race_distance'] == 3:
-            return constants.novice_full_limit
-
-    else:
-        if preferences['training_level_increase'] == 0:
-            return preferences['prior_miles_per_week']
-
-        else:
-            if preferences['race_distance'] == 0:
-                return constants.inter_5k_limit
-            if preferences['race_distance'] == 1:
-                return constants.inter_10k_limit
-            if preferences['race_distance'] == 2:
-                return constants.inter_half_limit
-            if preferences['race_distance'] == 3:
-                return constants.inter_full_limit
-
-
-def how_many_weeks_left(user):
-    """
-    Calculate the number of weeks until the race
-    """
-    preferences = get_preferences(user)
-    enddate = datetime.strptime(preferences['enddate'], '%Y-%m-%d')
-    today = datetime.today()
-    interval = enddate - today
-    total_days_left = interval.days
-    weeks_left = total_days_left/7
-    return weeks_left
-
-
-def days_per_week(preferences, weeks_of_plan):
+def generate_days_per_week(preferences, weeks_of_plan):
     """
     INPUTS
     max_days: number of days per week the user wants to run
@@ -196,8 +88,36 @@ def days_per_week(preferences, weeks_of_plan):
     return plan
 
 
-def construct_plan(days_per_week, miles_per_week, run_vector, weeks_of_plan):
+def build_plan(user):
+    """
+    Steps:
+    1. Decide how many days to run on each week up until race day
+    2. Decide how many miles to run on each week
+    3. Select appropriate proportion of run vector
+    4. Return plan, divided by week
+    """
 
-    training_plan = True
+    # Part 1: Read in preferences and appropriate run vector
+    preferences = utils.get_preferences(user)
+    run_vector = utils.get_run_vector(user)
+
+    # Part 2: Calculate weeks of plan
+    weeks_of_plan = lib.how_many_weeks_left(user)  # Jake
+
+    # Part 3: Generate number of days per week to run on each week
+    days_per_week = generate_days_per_week(preferences, weeks_of_plan)  # Holly
+
+    # Part 4: Generate mileage per week for each week
+    mileage_baseline = lib.get_baseline_mileage(preferences)
+    mileage_limit = lib.get_limit_mileage(preferences)
+
+    if mileage_limit <= mileage_baseline + 5:
+        mileage_limit = mileage_baseline + 5
+
+    miles_per_week = generate_mpw(mileage_baseline, mileage_limit, weeks_of_plan)
+
+    # Part 5: concatenate Part 3 & 4 to create full plan with run vector
+    training_plan = construct_plan(days_per_week, miles_per_week, run_vector, weeks_of_plan)
+    # Probably just put this bit within the function
 
     return training_plan
--- code/main/algorithm/lib.py
+++ code/main/algorithm/lib.py
@@ -1,30 +1,68 @@
-import json
-import pandas as pd
+from datetime import *
 
 
-def get_preferences(user):
+def get_baseline_mileage(preferences):
     """
-    Given user object, return dictionary of preferences from appropriate file ---- JAKE
-
-    NOTES:
-    -This is bad practice because I am hardcoding a filepath, but I think that is fine for this project -JT
+    1. Novice, no prior training - pre-defined constants
+    2. Novice, prior training - prior training levels
+    3. Intermediate - prior training levels
     """
-    preferences = json.load(open('../users/' + user + '/preferences.txt'))
-    return preferences
+
+    if 'prior_miles_per_week' in preferences.keys():
+        return preferences['prior_miles_per_week']
+    else:
+        if preferences['race_distance'] == 0:
+            return constants.novice_5k_baseline
+        if preferences['race_distance'] == 1:
+            return constants.novice_10k_baseline
+        if preferences['race_distance'] == 2:
+            return constants.novice_half_baseline
+        if preferences['race_distance'] == 3:
+            return constants.novice_full_baseline
 
 
-def get_run_vector(user):
+def get_limit_mileage(preferences):
     """
-    Obtain appropriate run vector given specific preferences ----- JAKE
+    1. Novice - pre-defined constants
+    2. Intermediate, keep same levels - set to previous level
+    3. Intermediate, increase level - pre-defined constants
+    """
 
-    NOTES:
-    -I changed the input of this function to be a user instead of distances.  The output is a dataframe -JT
-    -We should standardize the runner types and run levels so that the labels in the preferences match the names of the vector files -JT
+    if preferences['runner_type'] == 0:
+        if preferences['race_distance'] == 0:
+            return constants.novice_5k_limit
+        if preferences['race_distance'] == 1:
+            return constants.novice_10k_limit
+        if preferences['race_distance'] == 2:
+            return constants.novice_half_limit
+        if preferences['race_distance'] == 3:
+            return constants.novice_full_limit
+
+    else:
+        if preferences['training_level_increase'] == 0:
+            return preferences['prior_miles_per_week']
+
+        else:
+            if preferences['race_distance'] == 0:
+                return constants.inter_5k_limit
+            if preferences['race_distance'] == 1:
+                return constants.inter_10k_limit
+            if preferences['race_distance'] == 2:
+                return constants.inter_half_limit
+            if preferences['race_distance'] == 3:
+                return constants.inter_full_limit
+
+
+def how_many_weeks_left(user):
+    """
+    Calculate the number of weeks until the race
     """
     preferences = get_preferences(user)
-    runlevel = preferences['runlevel']
-    typerace = preferences['typerace']
-    run_vector = pd.read_csv('../plan_vectors/' + runlevel + '_' + typerace + '.csv')
-    return run_vector
+    enddate = datetime.strptime(preferences['enddate'], '%Y-%m-%d')
+    today = datetime.today()
+    interval = enddate - today
+    total_days_left = interval.days
+    weeks_left = total_days_left/7
+    return weeks_left
 
 
