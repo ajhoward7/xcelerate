c1654244525b392a3964e1204d5ee785069dc191
Alex Howard <Alex@Alexs-MacBook-Pro.local>
2018-04-19 13:18:05 -0700

    Changing code structure

--- code/main/algorithm/generate_plan.py
+++ code/main/algorithm/generate_plan.py
@@ -1,9 +1,7 @@
-import json # Used in get_preferences
-import pandas as pd # Used in get_run_vector
-
 from datetime import * # Used in how_many_weeks_left
 
 import constants
+import lib
 
 
 def generate_training_plan(user):
@@ -13,39 +11,14 @@ def generate_training_plan(user):
     2. Choose appropriate run vector
     3. Generate training plan
     """
-    preferences = get_preferences(user)  # Jake
+    preferences = lib.get_preferences(user)  # Jake
 
-    run_vector = get_run_vector(preferences['distance'], preferences['level'])  # Jake
+    run_vector = lib.get_run_vector(user)  # Jake
 
     training_plan = build_plan(preferences, run_vector)
 
     return training_plan
 
-
-def get_preferences(user):
-    """
-    Given user object, return dictionary of preferences from appropriate file ---- JAKE
-
-    NOTES:
-    -This is bad practice because I am hardcoding a filepath, but I think that is fine for this project -JT
-    """
-    preferences = json.load(open('../users/' + user + '/preferences.txt'))
-    return preferences
-
-
-def get_run_vector(user):
-    """
-    Obtain appropriate run vector given specific preferences ----- JAKE
-
-    NOTES:
-    -I changed the input of this function to be a user instead of distances.  The output is a dataframe -JT
-    -We should standardize the runner types and run levels so that the labels in the preferences match the names of the vector files -JT
-    """
-    preferences = get_preferences(user)
-    runlevel = preferences['runlevel']
-    typerace = preferences['typerace']
-    run_vector = pd.read_csv('../plan_vectors/' + runlevel + '_' + typerace + '.csv')
-    return run_vector
 
 def build_plan(preferences, run_vector):
     """
@@ -223,6 +196,8 @@ def days_per_week(preferences, weeks_of_plan):
     return plan
 
 
-def construct_plan(...):
+def construct_plan(days_per_week, miles_per_week, run_vector, weeks_of_plan):
 
-    return training plan
+    training_plan = True
+
+    return training_plan
---
+++ code/main/algorithm/lib.py
@@ -0,0 +1,30 @@
+import json
+import pandas as pd
+
+
+def get_preferences(user):
+    """
+    Given user object, return dictionary of preferences from appropriate file ---- JAKE
+
+    NOTES:
+    -This is bad practice because I am hardcoding a filepath, but I think that is fine for this project -JT
+    """
+    preferences = json.load(open('../users/' + user + '/preferences.txt'))
+    return preferences
+
+
+def get_run_vector(user):
+    """
+    Obtain appropriate run vector given specific preferences ----- JAKE
+
+    NOTES:
+    -I changed the input of this function to be a user instead of distances.  The output is a dataframe -JT
+    -We should standardize the runner types and run levels so that the labels in the preferences match the names of the vector files -JT
+    """
+    preferences = get_preferences(user)
+    runlevel = preferences['runlevel']
+    typerace = preferences['typerace']
+    run_vector = pd.read_csv('../plan_vectors/' + runlevel + '_' + typerace + '.csv')
+    return run_vector
+
+
