4ec16518a41b0e9409cd4f46f97912d6d17cb6d5
danaiavg <dsavgerinou@dons.usfca.edu>
2018-05-01 18:47:25 -0700

    tweaks

--- code/app.py
+++ code/app.py
@@ -409,5 +409,5 @@ def inputs(username):
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host = '0.0.0.0', port = 80)
-    # app.run()
+#    app.run(host = '0.0.0.0', port = 80)
+    app.run()
