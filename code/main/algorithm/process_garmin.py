a76fd441634540f68136cb7b0e7316fa8b6e2786
Holly Capell <hcapell@dons.usfca.edu>
2018-05-01 20:08:16 -0700

    holly update process_garmin.py

--- code/main/algorithm/process_garmin.py
+++ code/main/algorithm/process_garmin.py
@@ -1,6 +1,9 @@
 # Script to process Garmin files
 
 import pandas as pd
+import numpy as np
+import json
+import os
 
 filename = 'activities.csv'
 user = 'alex'
@@ -8,11 +11,35 @@ user = 'alex'
 training = pd.read_csv(filename)
 
 def update_preferences(training):
-	"""
-	Holly: update preferences with prior_miles_per_week and prior_days_per_week based on Garmin file
-	"""
+    """
+    Holly: update preferences with prior_miles_per_week and prior_days_per_week based on Garmin file
+    """
+    
+    training['Date'] = pd.to_datetime(training['Date'])
+    training['Week'] = training['Date'].dt.week
+    training['DOW'] = training['Date'].dt.day
+    
+    weeks = training.Week.unique()
+    last_3_weeks = weeks[0:3]
+    last_3_weeks_df = training.loc[training['Week'].isin(last_3_weeks)]
+    
+    days_per_week = last_3_weeks_df.groupby(['Week'], as_index = False)['DOW'].nunique()
+    prior_days_per_week = int(round((np.mean(days_per_week))))
+    
+    miles_per_week = last_3_weeks_df.groupby(['Week'], as_index = False)['Distance'].sum()
+    prior_miles_per_week = np.mean(miles_per_week['Distance'])
 
-	return True
+    filename = '../users/' + user + '/preferences.txt'
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        data['prior_days_per_week'] = prior_days_per_week
+        data['prior_miles_per_week'] = prior_miles_per_week
+
+    os.remove(filename)
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+    
+    return True
 
 
 def write_planned_training(training):
