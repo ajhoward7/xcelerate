967a0381e65777dc898642fdabc2b245e1b0f8fb
Jake Toffler <jake.toffler@gmail.com>
2018-05-04 00:05:31 -0700

    made logo bigger

--- code/app.py
+++ code/app.py
@@ -521,5 +521,5 @@ def foo(username):
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host = '0.0.0.0', port = 80)
-    # app.run()
+    # app.run(host = '0.0.0.0', port = 80)
+    app.run()
