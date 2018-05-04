60acc6cac800c8594ae57f7b3eb2174f46fc3cf6
t2liu <yliu266@usfca.edu>
2018-05-04 00:22:47 -0700

    pep 8 checked for generate_plan.py

--- code/main/algorithm/generate_plan.py
+++ code/main/algorithm/generate_plan.py
@@ -7,10 +7,11 @@ import lib
 import utils
 
 # For local testing:
-#import os
-#os.chdir('/Users/alexhoward/Dropbox/xcelerate/code/')
+# import os
+# os.chdir('/Users/alexhoward/Dropbox/xcelerate/code/')
 """
-This code is used to generate an initial training plan for our user, using the user preferences (stored as a dict) and
+This code is used to generate an initial training plan for our user,
+using the user preferences (stored as a dict) and
 our constants - specified in constants.py
 
 A training plan can be constructed by calling `generate_plan(user)`.
@@ -26,13 +27,16 @@ Method:
 
 def generate_mpw(mileage_baseline, mileage_limit, weeks, preferences):
     """
-    Given the preferences dictionary and number of weeks in the plan, this function returns a list of weekly mileages
+    Given the preferences dictionary and number of weeks in the plan,
+    this function returns a list of weekly mileages
     for the individual.
 
     Steps:
     1. Initialise to 0
-    2. Set baseline (previous training if applicable, else baseline defined in constants)
-    3. Increase by constant factor (`increase_factor`) every `increase_period` number of weeks
+    2. Set baseline (previous training if applicable,
+       else baseline defined in constants)
+    3. Increase by constant factor (`increase_factor`),
+       every `increase_period` number of weeks
     4. Adjust weekly mileage down every `easy_week_frequency` number of weeks
     5. Adjust mileage for final two weeks (TO DO)
     """
@@ -42,11 +46,14 @@ def generate_mpw(mileage_baseline, mileage_limit, weeks, preferences):
 
     for i in range(weeks):
         miles_per_week[i] = current_mileage
-        if i % constants.increase_period == 0 and current_mileage < mileage_limit and preferences['runner_type'] == 0:
-            current_mileage *= (1+constants.increase_factor_novice)
-        if i % constants.increase_period == 0 and current_mileage < mileage_limit and preferences['runner_type'] == 0:
-            current_mileage *= (1+constants.increase_factor_inter)
-
+        if i % constants.increase_period == 0 \
+            and current_mileage < mileage_limit \
+                and preferences['runner_type'] == 0:
+                    current_mileage *= (1+constants.increase_factor_novice)
+        if i % constants.increase_period == 0 \
+            and current_mileage < mileage_limit \
+                and preferences['runner_type'] == 0:
+                    current_mileage *= (1+constants.increase_factor_inter)
 
     for i in range(weeks):
         if i+1 % constants.easy_week_frequency == 0:
@@ -63,10 +70,12 @@ def generate_mpw(mileage_baseline, mileage_limit, weeks, preferences):
 
 def generate_days_per_week(preferences, weeks):
     """
-    Given the preferences dictionary and the number of weeks in the plan, this function returns
-    a list of the number of training days per week for each week of the training plan.
+    Given the preferences dictionary and the number of weeks in the plan,
+    this function returns a list of the number of training days per week
+    for each week of the training plan.
 
-    Number of days per week initialised and then increased every 2nd week until reach maximum.
+    Number of days per week initialised and then increased every 2nd week
+    until reach maximum.
 
     -- PERHAPS CHANGE THIS TO MATCH MILES_PER_WEEK FUNCTION
     """
@@ -95,7 +104,8 @@ def generate_days_per_week(preferences, weeks):
         else:
             initial = int(round(max_days * .666))
 
-    if initial > max_days:  # Handles edge case of previous days per week > max_days
+    # Handles edge case of previous days per week > max_days
+    if initial > max_days:
         initial = max_days
 
     if not increase:
@@ -122,14 +132,16 @@ def generate_days_per_week(preferences, weeks):
 
     plan[-1] += -1  # One less day training on the last week
 
-    plan = [max(days,1) for days in plan]
+    plan = [max(days, 1) for days in plan]
 
     return plan
 
 
-def combine_miles_days(miles_per_week, days_per_week, preferences, run_vector, today):
+def combine_miles_days(miles_per_week, days_per_week,
+                       preferences, run_vector, today):
     """
-    Take MpW and days per week inputs and output training plan appropriate from run vector
+    Take MpW and days per week inputs and output training
+    plan appropriate from run vector
     """
 
     number_of_runs = sum(days_per_week) + 1
@@ -139,14 +151,14 @@ def combine_miles_days(miles_per_week, days_per_week, preferences, run_vector, t
     # Deal with edge case that run vector not long enough:
     while run_vector_length < number_of_runs:
         second_half_training = run_vector[int(run_vector_length/2):]
-        run_vector = pd.concat([run_vector,second_half_training])
+        run_vector = pd.concat([run_vector, second_half_training])
         run_vector_length = len(run_vector)
         run_vector['runs_to_race'] = range(run_vector_length)
 
     # Take relevant portion of run vectors (and reverse order):
     run_miles = list(run_vector['run_miles'])[:number_of_runs][::-1]
-    #run_workout = run_vector['workout'][:number_of_runs][::-1]
-    #run_long_run = run_vector['long_run'][:number_of_runs][::-1]
+    # run_workout = run_vector['workout'][:number_of_runs][::-1]
+    # run_long_run = run_vector['long_run'][:number_of_runs][::-1]
 
     adj_run_vector = []
     run_day = []
@@ -154,24 +166,28 @@ def combine_miles_days(miles_per_week, days_per_week, preferences, run_vector, t
 
     k = 0
 
-    start_of_week = today - timedelta(days=today.weekday())  # Initialise start_of_week
+    # Initialise start_of_week
+    start_of_week = today - timedelta(days=today.weekday())
 
-    available_days = preferences['available_days']  # Days available to run
+    # Days available to run
+    available_days = preferences['available_days']
 
     np.random.seed(666)  # Set seed for reproducibility when plan updated
 
     # Loop through each week and place days as appropriate:
     for i in range(len(days_per_week)):
-        days_this_week = max(days_per_week[i],1)
+        days_this_week = max(days_per_week[i], 1)
         miles_this_week = miles_per_week[i]
         runs_this_week = []
 
         if days_this_week <= len(available_days):
             np.random.seed(start_of_week.day + start_of_week.month)
-            which_days_this_week = np.sort(np.random.choice(available_days, days_this_week, False))
+            which_days_this_week = np.sort(np.random.choice(available_days,
+                                           days_this_week, False))
 
         else:
-            which_days_this_week = np.sort(np.random.choice(range(7), days_this_week, False))
+            which_days_this_week = np.sort(np.random.choice(range(7),
+                                           days_this_week, False))
 
         for j in range(days_this_week):
             runs_this_week.append(run_miles[k])
@@ -179,12 +195,12 @@ def combine_miles_days(miles_per_week, days_per_week, preferences, run_vector, t
             if which_days_this_week[j] == 0:
                 run_date.append(start_of_week)
             else:
-                run_date.append(start_of_week + timedelta(days=int(which_days_this_week[j])))
+                run_date.append(start_of_week + timedelta(days=int(
+                                which_days_this_week[j])))
 
             k += 1
 
         factor = float(miles_this_week) / sum(runs_this_week)
-
 
         max_val = np.max(runs_this_week)
         max_arg = np.argmax(runs_this_week)
@@ -200,16 +216,19 @@ def combine_miles_days(miles_per_week, days_per_week, preferences, run_vector, t
 
     # Format output:
     training_plan = pd.DataFrame([])
-    training_plan['miles'] = [max(run,1.5) for run in adj_run_vector]  # Hack to make sure no 0's
+    # Hack to make sure no 0's
+    training_plan['miles'] = [max(run, 1.5) for run in adj_run_vector]
     training_plan['run_day'] = run_day
     training_plan['run_date'] = [this_date for this_date in run_date]
-    #training_plan['workout'] = run_workout[:len(run_date)][::-1]
-    #training_plan['long_run'] = run_long_run[:len(run_date)][::-1]
-    training_plan['week_start'] = training_plan.run_date.apply(lambda x : (x - timedelta(days=x.weekday())))
+    # training_plan['workout'] = run_workout[:len(run_date)][::-1]
+    # training_plan['long_run'] = run_long_run[:len(run_date)][::-1]
+    training_plan['week_start'] = \
+        training_plan.run_date.apply(lambda x: (x -
+                                     timedelta(days=x.weekday())))
 
     race_day = datetime.strptime(preferences['race_date'], '%Y-%m-%d').date()
-
-    training_plan = training_plan[training_plan['run_date'] > today] # May want to omit this later
+    # May want to omit this later
+    training_plan = training_plan[training_plan['run_date'] > today]
     training_plan = training_plan[training_plan['run_date'] <= race_day]
 
     training_plan.miles = training_plan['miles'].astype('float')
@@ -217,13 +236,14 @@ def combine_miles_days(miles_per_week, days_per_week, preferences, run_vector, t
     training_plan = training_plan[:-1]
 
     # Adjust final entry to have correct race distance:
-    training_plan = training_plan.append({'miles': lib.get_race_distance(preferences),
-                                          'run_date': race_day,
-                                          'run_day': race_day.weekday(),
-                                          'week_start': race_day - timedelta(days = race_day.weekday())
-                                          #  ,'workout':0,'long_run':0
-                                          },
-                                            ignore_index = True)
+    training_plan = \
+        training_plan.append({'miles': lib.get_race_distance(preferences),
+                              'run_date': race_day,
+                              'run_day': race_day.weekday(),
+                              'week_start': race_day -
+                              timedelta(days=race_day.weekday())
+                              },
+                             ignore_index=True)
 
     return training_plan
 
@@ -256,16 +276,19 @@ def build_plan(user):
     if mileage_limit <= mileage_baseline + 5:
         mileage_limit = mileage_baseline + 5
 
-    miles_per_week = generate_mpw(mileage_baseline, mileage_limit, weeks, preferences)
+    miles_per_week = generate_mpw(mileage_baseline,
+                                  mileage_limit, weeks, preferences)
     assert(len(miles_per_week) == weeks)  # De-bugging
     # Part 5: concatenate Part 3 & 4 to create full plan with run vector
 
-    return combine_miles_days(miles_per_week, days_per_week, preferences, run_vector, date.today())
+    return combine_miles_days(miles_per_week, days_per_week,
+                              preferences, run_vector, date.today())
 
 
 def generate_plan(user):
     training_plan = build_plan(user)
-    training_plan.to_csv('main/users/{}/planned_training.csv'.format(user), index = False)
+    training_plan.to_csv('main/users/{}/planned_training.csv'.format(user),
+                         index=False)
 
     return training_plan
 
@@ -274,4 +297,5 @@ if __name__ == "__main__":
 
     user = '441'  # Adapt this
     training_plan = build_plan(user)
-    training_plan.to_csv('main/users/{}/planned_training.csv'.format(user), index = False)
+    training_plan.to_csv('main/users/{}/planned_training.csv'.format(user),
+                         index=False)
