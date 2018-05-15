85df908e9a4f35d109321d48de257bfe0679406f
danaiavg <dsavgerinou@dons.usfca.edu>
2018-05-14 21:36:39 -0700

    final changes everything works for xcelerate.run

--- code/app.py
+++ code/app.py
@@ -647,6 +647,6 @@ def redirectmap(username, last_date):
 
 
 if __name__ == "__main__":
-    app.debug = True
-    # app.run(host='0.0.0.0', port=80)
+    # app.debug = True
+    app.run(host='0.0.0.0', port=80)
     app.run()
